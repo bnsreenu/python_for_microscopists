9a2a5a1cffefc4074c6756ed8814e6b5c300a53b
Sreenivas Bhattiprolu <19365893+bnsreenu@users.noreply.github.com>
2020-10-05 18:03:33 -0700

    Add files via upload

---
+++ 164a-Intro_to_time_series_Forecasting_using_feed_forward_NN.py
@@ -0,0 +1,149 @@
+# https://youtu.be/tKM5d8Ll1k0
+"""
+Dataset from: https://www.kaggle.com/rakannimer/air-passengers
+International Airline Passengers prediction problem.
+This is a problem where, given a year and a month, the task is to predict 
+the number of international airline passengers in units of 1,000. 
+The data ranges from January 1949 to December 1960, or 12 years, with 144 observations.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+# load the dataset
+dataframe = read_csv('data/AirPassengers.csv', usecols=[1])
+plt.plot(dataframe)
+
+#Convert pandas dataframe to numpy array
+dataset = dataframe.values
+dataset = dataset.astype('float32') #COnvert values to float
+
+# Normalization is optional but recommended for neural network as certain 
+# activation functions are sensitive to magnitude of numbers. 
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1)) #Also try QuantileTransformer
+dataset = scaler.fit_transform(dataset)
+
+#We cannot use random way of splitting dataset into train and test as
+#the sequence of events is important for time series.
+#So let us take first 60% values for train and the remaining 1/3 for testing
+# split into train and test sets
+train_size = int(len(dataset) * 0.66)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+
+
+
+# We cannot fit the model like we normally do for image processing where we have
+#X and Y. We need to transform our data into something that looks like X and Y values.
+# This way it can be trained on a sequence rather than indvidual datapoints. 
+# Let us convert into n number of columns for X where we feed sequence of numbers
+#then the final column as Y where we provide the next number in the sequence as output.
+# So let us convert an array of values into a dataset matrix
+
+#seq_size is the number of previous time steps to use as 
+#input variables to predict the next time period.
+
+#creates a dataset where X is the number of passengers at a given time (t, t-1, t-2...) 
+#and Y is the number of passengers at the next time (t + 1).
+
+def to_sequences(dataset, seq_size=1):
+    x = []
+    y = []
+
+    for i in range(len(dataset)-seq_size-1):
+        #print(i)
+        window = dataset[i:(i+seq_size), 0]
+        x.append(window)
+        y.append(dataset[i+seq_size, 0])
+        
+    return np.array(x),np.array(y)
+    
+
+seq_size = 10 # Number of time steps to look back 
+#Larger sequences (look further back) may improve forecasting.
+trainX, trainY = to_sequences(train, seq_size)
+testX, testY = to_sequences(test, seq_size)
+
+#Compare trainX and dataset. You can see that X= values at t, t+1 and t+2
+#whereas Y is the value that follows, t+3 (since our sequence size is 3)
+
+print("Shape of training set: {}".format(trainX.shape))
+print("Shape of test set: {}".format(testX.shape))
+
+#Input dimensions are... (N x seq_size)
+print('Build deep model...')
+# create and fit dense model
+model = Sequential()
+model.add(Dense(64, input_dim=seq_size, activation='relu')) #12
+model.add(Dense(32, activation='relu'))  #8
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam', metrics = ['acc'])
+print(model.summary()) 
+
+#for layer in model.layers:
+#    print(layer.input_shape)
+
+##################################################
+#Try another model....
+#print('Build simple model...')
+## create and fit dense model
+#model = Sequential()
+#model.add(Dense(8, input_dim=seq_size, activation='relu'))
+#model.add(Dense(1))
+#model.compile(loss='mean_squared_error', optimizer='adam', metrics = ['acc'])
+#print(model.summary()) 
+
+
+
+#model.fit(trainX, trainY, epochs=200, batch_size=2, verbose=2)
+
+model.fit(trainX, trainY, validation_data=(testX, testY),
+          verbose=2, epochs=100)
+
+
+
+# make predictions
+
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+# Estimate model performance
+#SInce we used minmaxscaler we can now use scaler.inverse_transform
+#to invert the transformation.
+
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY_inverse = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY_inverse = scaler.inverse_transform([testY])
+
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY_inverse[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+
+testScore = math.sqrt(mean_squared_error(testY_inverse[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+#we must shift the predictions so that they align on the x-axis with the original dataset. 
+trainPredictPlot = np.empty_like(dataset)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[seq_size:len(trainPredict)+seq_size, :] = trainPredict
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(trainPredict)+(seq_size*2)+1:len(dataset)-1, :] = testPredict
+
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
