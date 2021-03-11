a12432ef86f48067c9e4392c32776d69e104edfc
Sreenivas Bhattiprolu <19365893+bnsreenu@users.noreply.github.com>
2021-03-11 07:52:23 -0800

    Add files via upload

---
+++ 208_multiclass_Unet_sandstone.py
@@ -0,0 +1,261 @@
+# https://youtu.be/XyX5HNuv-xE
+"""
+Author: Dr. Sreenivas Bhattiprolu
+
+Multiclass semantic segmentation using U-Net
+
+Including segmenting large images by dividing them into smaller patches 
+and stiching them back
+
+To annotate images and generate labels, you can use APEER (for free):
+www.apeer.com 
+"""
+
+from simple_multi_unet_model import multi_unet_model #Uses softmax 
+
+from keras.utils import normalize
+import os
+import glob
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+#Resizing images, if needed
+SIZE_X = 128 
+SIZE_Y = 128
+n_classes=4 #Number of classes for segmentation
+
+#Capture training image info as a list
+train_images = []
+
+for directory_path in glob.glob("128_patches/images/"):
+    for img_path in glob.glob(os.path.join(directory_path, "*.tif")):
+        img = cv2.imread(img_path, 0)       
+        #img = cv2.resize(img, (SIZE_Y, SIZE_X))
+        train_images.append(img)
+       
+#Convert list to array for machine learning processing        
+train_images = np.array(train_images)
+
+#Capture mask/label info as a list
+train_masks = [] 
+for directory_path in glob.glob("128_patches/masks/"):
+    for mask_path in glob.glob(os.path.join(directory_path, "*.tif")):
+        mask = cv2.imread(mask_path, 0)       
+        #mask = cv2.resize(mask, (SIZE_Y, SIZE_X), interpolation = cv2.INTER_NEAREST)  #Otherwise ground truth changes due to interpolation
+        train_masks.append(mask)
+        
+#Convert list to array for machine learning processing          
+train_masks = np.array(train_masks)
+
+###############################################
+#Encode labels... but multi dim array so need to flatten, encode and reshape
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+n, h, w = train_masks.shape
+train_masks_reshaped = train_masks.reshape(-1,1)
+train_masks_reshaped_encoded = labelencoder.fit_transform(train_masks_reshaped)
+train_masks_encoded_original_shape = train_masks_reshaped_encoded.reshape(n, h, w)
+
+np.unique(train_masks_encoded_original_shape)
+
+#################################################
+train_images = np.expand_dims(train_images, axis=3)
+train_images = normalize(train_images, axis=1)
+
+train_masks_input = np.expand_dims(train_masks_encoded_original_shape, axis=3)
+
+#Create a subset of data for quick testing
+#Picking 10% for testing and remaining for training
+from sklearn.model_selection import train_test_split
+X1, X_test, y1, y_test = train_test_split(train_images, train_masks_input, test_size = 0.10, random_state = 0)
+
+#Further split training data t a smaller subset for quick testing of models
+X_train, X_do_not_use, y_train, y_do_not_use = train_test_split(X1, y1, test_size = 0.2, random_state = 0)
+
+print("Class values in the dataset are ... ", np.unique(y_train))  # 0 is the background/few unlabeled 
+
+from keras.utils import to_categorical
+train_masks_cat = to_categorical(y_train, num_classes=n_classes)
+y_train_cat = train_masks_cat.reshape((y_train.shape[0], y_train.shape[1], y_train.shape[2], n_classes))
+
+
+
+test_masks_cat = to_categorical(y_test, num_classes=n_classes)
+y_test_cat = test_masks_cat.reshape((y_test.shape[0], y_test.shape[1], y_test.shape[2], n_classes))
+
+
+
+###############################################################
+from sklearn.utils import class_weight
+class_weights = class_weight.compute_class_weight('balanced',
+                                                 np.unique(train_masks_reshaped_encoded),
+                                                 train_masks_reshaped_encoded)
+print("Class weights are...:", class_weights)
+
+
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH  = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3]
+
+def get_model():
+    return multi_unet_model(n_classes=n_classes, IMG_HEIGHT=IMG_HEIGHT, IMG_WIDTH=IMG_WIDTH, IMG_CHANNELS=IMG_CHANNELS)
+
+model = get_model()
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+#If starting with pre-trained weights. 
+#model.load_weights('???.hdf5')
+
+history = model.fit(X_train, y_train_cat, 
+                    batch_size = 16, 
+                    verbose=1, 
+                    epochs=50, 
+                    validation_data=(X_test, y_test_cat), 
+                    #class_weight=class_weights,
+                    shuffle=False)
+                    
+
+
+model.save('test.hdf5')
+#model.save('sandstone_50_epochs_catXentropy_acc_with_weights.hdf5')
+############################################################
+#Evaluate the model
+	# evaluate model
+_, acc = model.evaluate(X_test, y_test_cat)
+print("Accuracy is = ", (acc * 100.0), "%")
+
+
+###
+#plot the training and validation accuracy and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+plt.plot(epochs, acc, 'y', label='Training Accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation Accuracy')
+plt.title('Training and validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+
+##################################
+#model = get_model()
+model.load_weights('sandstone_50_epochs_catXentropy_acc.hdf5')  
+#model.load_weights('sandstone_50_epochs_catXentropy_acc_with_weights.hdf5')  
+
+#IOU
+y_pred=model.predict(X_test)
+y_pred_argmax=np.argmax(y_pred, axis=3)
+
+##################################################
+
+#Using built in keras function
+from keras.metrics import MeanIoU
+n_classes = 4
+IOU_keras = MeanIoU(num_classes=n_classes)  
+IOU_keras.update_state(y_test[:,:,:,0], y_pred_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+
+#To calculate I0U for each class...
+values = np.array(IOU_keras.get_weights()).reshape(n_classes, n_classes)
+print(values)
+class1_IoU = values[0,0]/(values[0,0] + values[0,1] + values[0,2] + values[0,3] + values[1,0]+ values[2,0]+ values[3,0])
+class2_IoU = values[1,1]/(values[1,1] + values[1,0] + values[1,2] + values[1,3] + values[0,1]+ values[2,1]+ values[3,1])
+class3_IoU = values[2,2]/(values[2,2] + values[2,0] + values[2,1] + values[2,3] + values[0,2]+ values[1,2]+ values[3,2])
+class4_IoU = values[3,3]/(values[3,3] + values[3,0] + values[3,1] + values[3,2] + values[0,3]+ values[1,3]+ values[2,3])
+
+print("IoU for class1 is: ", class1_IoU)
+print("IoU for class2 is: ", class2_IoU)
+print("IoU for class3 is: ", class3_IoU)
+print("IoU for class4 is: ", class4_IoU)
+
+plt.imshow(train_images[0, :,:,0], cmap='gray')
+plt.imshow(train_masks[0], cmap='gray')
+#######################################################################
+#Predict on a few images
+#model = get_model()
+#model.load_weights('???.hdf5')  
+import random
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,0][:,:,None]
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input))
+predicted_img=np.argmax(prediction, axis=3)[0,:,:]
+
+
+plt.figure(figsize=(12, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='jet')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(predicted_img, cmap='jet')
+plt.show()
+
+#####################################################################
+
+#Predict on large image
+
+#Apply a trained model on large image
+
+from patchify import patchify, unpatchify
+
+large_image = cv2.imread('large_images/large_image.tif', 0)
+#This will split the image into small images of shape [3,3]
+patches = patchify(large_image, (128, 128), step=128)  #Step=256 for 256 patches means no overlap
+
+predicted_patches = []
+for i in range(patches.shape[0]):
+    for j in range(patches.shape[1]):
+        print(i,j)
+        
+        single_patch = patches[i,j,:,:]       
+        single_patch_norm = np.expand_dims(normalize(np.array(single_patch), axis=1),2)
+        single_patch_input=np.expand_dims(single_patch_norm, 0)
+        single_patch_prediction = (model.predict(single_patch_input))
+        single_patch_predicted_img=np.argmax(single_patch_prediction, axis=3)[0,:,:]
+
+        predicted_patches.append(single_patch_predicted_img)
+
+predicted_patches = np.array(predicted_patches)
+
+predicted_patches_reshaped = np.reshape(predicted_patches, (patches.shape[0], patches.shape[1], 128,128) )
+
+reconstructed_image = unpatchify(predicted_patches_reshaped, large_image.shape)
+plt.imshow(reconstructed_image, cmap='gray')
+#plt.imsave('data/results/segm.jpg', reconstructed_image, cmap='gray')
+
+plt.hist(reconstructed_image.flatten())  #Threshold everything above 0
+
+# final_prediction = (reconstructed_image > 0.01).astype(np.uint8)
+# plt.imshow(final_prediction)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(221)
+plt.title('Large Image')
+plt.imshow(large_image, cmap='gray')
+plt.subplot(222)
+plt.title('Prediction of large Image')
+plt.imshow(reconstructed_image, cmap='jet')
+plt.show()
