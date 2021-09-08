4ae8c0e2575f1a7cb638ca2b4b222f4cb58b6250
Dr. Sreenivas Bhattiprolu <19365893+bnsreenu@users.noreply.github.com>
2021-09-08 05:05:46 -0700

    Add files via upload

---
+++ 231_234_BraTa2020_Unet_segmentation/234_train_brats2020_V5.0.py
@@ -0,0 +1,311 @@
+# https://youtu.be/ScdCQqLtnis
+"""
+@author: Sreenivas Bhattiprolu
+
+Code to train batches of cropped BraTS 2020 images using 3D U-net.
+
+Please get the data ready and define custom data gnerator using the other
+files in this directory.
+
+Images are expected to be 128x128x128x3 npy data (3 corresponds to the 3 channels for 
+                                                  test_image_flair, test_image_t1ce, test_image_t2)
+Change the U-net input shape based on your input dataset shape (e.g. if you decide to only se 2 channels or all 4 channels)
+
+Masks are expected to be 128x128x128x3 npy data (4 corresponds to the 4 classes / labels)
+
+
+You can change input image sizes to customize for your computing resources.
+"""
+
+
+import os
+import numpy as np
+from custom_datagen import imageLoader
+#import tensorflow as tf
+import keras
+from matplotlib import pyplot as plt
+import glob
+import random
+
+
+
+####################################################
+train_img_dir = "BraTS2020_TrainingData/input_data_128/train/images/"
+train_mask_dir = "BraTS2020_TrainingData/input_data_128/train/masks/"
+
+img_list = os.listdir(train_img_dir)
+msk_list = os.listdir(train_mask_dir)
+
+num_images = len(os.listdir(train_img_dir))
+
+img_num = random.randint(0,num_images-1)
+test_img = np.load(train_img_dir+img_list[img_num])
+test_mask = np.load(train_mask_dir+msk_list[img_num])
+test_mask = np.argmax(test_mask, axis=3)
+
+n_slice=random.randint(0, test_mask.shape[2])
+plt.figure(figsize=(12, 8))
+
+plt.subplot(221)
+plt.imshow(test_img[:,:,n_slice, 0], cmap='gray')
+plt.title('Image flair')
+plt.subplot(222)
+plt.imshow(test_img[:,:,n_slice, 1], cmap='gray')
+plt.title('Image t1ce')
+plt.subplot(223)
+plt.imshow(test_img[:,:,n_slice, 2], cmap='gray')
+plt.title('Image t2')
+plt.subplot(224)
+plt.imshow(test_mask[:,:,n_slice])
+plt.title('Mask')
+plt.show()
+
+#############################################################
+#Optional step of finding the distribution of each class and calculating appropriate weights
+#Alternatively you can just assign equal weights and see how well the model performs: 0.25, 0.25, 0.25, 0.25
+
+import pandas as pd
+columns = ['0','1', '2', '3']
+df = pd.DataFrame(columns=columns)
+train_mask_list = sorted(glob.glob('BraTS2020_TrainingData/input_data_128/train/masks/*.npy'))
+for img in range(len(train_mask_list)):
+    print(img)
+    temp_image=np.load(train_mask_list[img])
+    temp_image = np.argmax(temp_image, axis=3)
+    val, counts = np.unique(temp_image, return_counts=True)
+    zipped = zip(columns, counts)
+    conts_dict = dict(zipped)
+    
+    df = df.append(conts_dict, ignore_index=True)
+
+label_0 = df['0'].sum()
+label_1 = df['1'].sum()
+label_2 = df['1'].sum()
+label_3 = df['3'].sum()
+total_labels = label_0 + label_1 + label_2 + label_3
+n_classes = 4
+#Class weights claculation: n_samples / (n_classes * n_samples_for_class)
+wt0 = round((total_labels/(n_classes*label_0)), 2) #round to 2 decimals
+wt1 = round((total_labels/(n_classes*label_1)), 2)
+wt2 = round((total_labels/(n_classes*label_2)), 2)
+wt3 = round((total_labels/(n_classes*label_3)), 2)
+
+#Weights are: 0.26, 22.53, 22.53, 26.21
+#wt0, wt1, wt2, wt3 = 0.26, 22.53, 22.53, 26.21
+#These weihts can be used for Dice loss 
+
+##############################################################
+#Define the image generators for training and validation
+
+train_img_dir = "BraTS2020_TrainingData/input_data_128/train/images/"
+train_mask_dir = "BraTS2020_TrainingData/input_data_128/train/masks/"
+
+val_img_dir = "BraTS2020_TrainingData/input_data_128/val/images/"
+val_mask_dir = "BraTS2020_TrainingData/input_data_128/val/masks/"
+
+train_img_list=os.listdir(train_img_dir)
+train_mask_list = os.listdir(train_mask_dir)
+
+val_img_list=os.listdir(val_img_dir)
+val_mask_list = os.listdir(val_mask_dir)
+##################################
+
+########################################################################
+batch_size = 2
+
+train_img_datagen = imageLoader(train_img_dir, train_img_list, 
+                                train_mask_dir, train_mask_list, batch_size)
+
+val_img_datagen = imageLoader(val_img_dir, val_img_list, 
+                                val_mask_dir, val_mask_list, batch_size)
+
+#Verify generator.... In python 3 next() is renamed as __next__()
+img, msk = train_img_datagen.__next__()
+
+img_num = random.randint(0,img.shape[0]-1)
+test_img=img[img_num]
+test_mask=msk[img_num]
+test_mask=np.argmax(test_mask, axis=3)
+
+n_slice=random.randint(0, test_mask.shape[2])
+plt.figure(figsize=(12, 8))
+
+plt.subplot(221)
+plt.imshow(test_img[:,:,n_slice, 0], cmap='gray')
+plt.title('Image flair')
+plt.subplot(222)
+plt.imshow(test_img[:,:,n_slice, 1], cmap='gray')
+plt.title('Image t1ce')
+plt.subplot(223)
+plt.imshow(test_img[:,:,n_slice, 2], cmap='gray')
+plt.title('Image t2')
+plt.subplot(224)
+plt.imshow(test_mask[:,:,n_slice])
+plt.title('Mask')
+plt.show()
+
+
+###########################################################################
+#Define loss, metrics and optimizer to be used for training
+wt0, wt1, wt2, wt3 = 0.25,0.25,0.25,0.25
+import segmentation_models_3D as sm
+dice_loss = sm.losses.DiceLoss(class_weights=np.array([wt0, wt1, wt2, wt3])) 
+focal_loss = sm.losses.CategoricalFocalLoss()
+total_loss = dice_loss + (1 * focal_loss)
+
+metrics = ['accuracy', sm.metrics.IOUScore(threshold=0.5)]
+
+LR = 0.0001
+optim = keras.optimizers.Adam(LR)
+#######################################################################
+#Fit the model 
+
+steps_per_epoch = len(train_img_list)//batch_size
+val_steps_per_epoch = len(val_img_list)//batch_size
+
+
+from  simple_3d_unet import simple_unet_model
+
+model = simple_unet_model(IMG_HEIGHT=128, 
+                          IMG_WIDTH=128, 
+                          IMG_DEPTH=128, 
+                          IMG_CHANNELS=3, 
+                          num_classes=4)
+
+model.compile(optimizer = optim, loss=total_loss, metrics=metrics)
+print(model.summary())
+
+print(model.input_shape)
+print(model.output_shape)
+
+history=model.fit(train_img_datagen,
+          steps_per_epoch=steps_per_epoch,
+          epochs=100,
+          verbose=1,
+          validation_data=val_img_datagen,
+          validation_steps=val_steps_per_epoch,
+          )
+
+model.save('brats_3d.hdf5')
+##################################################################
+
+
+#plot the training and validation IoU and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+plt.plot(epochs, acc, 'y', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+#################################################
+from keras.models import load_model
+
+#Load model for prediction or continue training
+
+#For continuing training....
+#The following gives an error: Unknown loss function: dice_loss_plus_1focal_loss
+#This is because the model does not save loss function and metrics. So to compile and 
+#continue training we need to provide these as custom_objects.
+my_model = load_model('saved_models/brats_3d_100epochs_simple_unet_weighted_dice.hdf5')
+
+#So let us add the loss as custom object... but the following throws another error...
+#Unknown metric function: iou_score
+my_model = load_model('saved_models/brats_3d_100epochs_simple_unet_weighted_dice.hdf5', 
+                      custom_objects={'dice_loss_plus_1focal_loss': total_loss})
+
+#Now, let us add the iou_score function we used during our initial training
+my_model = load_model('saved_models/brats_3d_100epochs_simple_unet_weighted_dice.hdf5', 
+                      custom_objects={'dice_loss_plus_1focal_loss': total_loss,
+                                      'iou_score':sm.metrics.IOUScore(threshold=0.5)})
+
+#Now all set to continue the training process. 
+history2=my_model.fit(train_img_datagen,
+          steps_per_epoch=steps_per_epoch,
+          epochs=1,
+          verbose=1,
+          validation_data=val_img_datagen,
+          validation_steps=val_steps_per_epoch,
+          )
+#################################################
+
+#For predictions you do not need to compile the model, so ...
+my_model = load_model('saved_models/brats_3d_100epochs_simple_unet_weighted_dice.hdf5', 
+                      compile=False)
+
+
+#Verify IoU on a batch of images from the test dataset
+#Using built in keras function for IoU
+#Only works on TF > 2.0
+from keras.metrics import MeanIoU
+
+batch_size=8 #Check IoU for a batch of images
+test_img_datagen = imageLoader(val_img_dir, val_img_list, 
+                                val_mask_dir, val_mask_list, batch_size)
+
+#Verify generator.... In python 3 next() is renamed as __next__()
+test_image_batch, test_mask_batch = test_img_datagen.__next__()
+
+test_mask_batch_argmax = np.argmax(test_mask_batch, axis=4)
+test_pred_batch = my_model.predict(test_image_batch)
+test_pred_batch_argmax = np.argmax(test_pred_batch, axis=4)
+
+n_classes = 4
+IOU_keras = MeanIoU(num_classes=n_classes)  
+IOU_keras.update_state(test_pred_batch_argmax, test_mask_batch_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+#############################################
+#Predict on a few test images, one at a time
+#Try images: 
+img_num = 82
+
+test_img = np.load("BraTS2020_TrainingData/input_data_128/val/images/image_"+str(img_num)+".npy")
+
+test_mask = np.load("BraTS2020_TrainingData/input_data_128/val/masks/mask_"+str(img_num)+".npy")
+test_mask_argmax=np.argmax(test_mask, axis=3)
+
+test_img_input = np.expand_dims(test_img, axis=0)
+test_prediction = my_model.predict(test_img_input)
+test_prediction_argmax=np.argmax(test_prediction, axis=4)[0,:,:,:]
+
+
+# print(test_prediction_argmax.shape)
+# print(test_mask_argmax.shape)
+# print(np.unique(test_prediction_argmax))
+
+
+#Plot individual slices from test predictions for verification
+from matplotlib import pyplot as plt
+import random
+
+#n_slice=random.randint(0, test_prediction_argmax.shape[2])
+n_slice = 55
+plt.figure(figsize=(12, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,n_slice,1], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(test_mask_argmax[:,:,n_slice])
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(test_prediction_argmax[:,:, n_slice])
+plt.show()
+
+############################################################
+
---
+++ 231_234_BraTa2020_Unet_segmentation/simple_3d_unet.py
@@ -0,0 +1,85 @@
+# https://youtu.be/ScdCQqLtnis
+"""
+@author: Sreenivas Bhattiprolu
+
+Converted my standard 2D Unet to 3D. 
+
+"""
+
+from keras.models import Model
+from keras.layers import Input, Conv3D, MaxPooling3D, concatenate, Conv3DTranspose, BatchNormalization, Dropout, Lambda
+from keras.optimizers import Adam
+from keras.metrics import MeanIoU
+
+kernel_initializer =  'he_uniform' #Try others if you want
+
+
+################################################################
+def simple_unet_model(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classes):
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS))
+    #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
+    s = inputs
+
+    #Contraction path
+    c1 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c1)
+    p1 = MaxPooling3D((2, 2, 2))(c1)
+    
+    c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c2)
+    p2 = MaxPooling3D((2, 2, 2))(c2)
+     
+    c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c3)
+    p3 = MaxPooling3D((2, 2, 2))(c3)
+     
+    c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c4)
+    p4 = MaxPooling3D(pool_size=(2, 2, 2))(c4)
+     
+    c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv3D(256, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv3D(128, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c6)
+     
+    u7 = Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv3D(64, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c7)
+     
+    u8 = Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv3D(32, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c8)
+     
+    u9 = Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1])
+    c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv3D(16, (3, 3, 3), activation='relu', kernel_initializer=kernel_initializer, padding='same')(c9)
+     
+    outputs = Conv3D(num_classes, (1, 1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    #compile model outside of this function to make it flexible. 
+    model.summary()
+    
+    return model
+
+#Test if everything is working ok. 
+model = simple_unet_model(128, 128, 128, 3, 4)
+print(model.input_shape)
+print(model.output_shape)
